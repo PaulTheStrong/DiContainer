9fa4658d40c42b50ff1f76b1d37143abc1bbb630
PaulTheStrong <98765432123456789r@gmail.com>
2021-12-15 17:03:19 +0300

    bean configuration

---
+++ DIContainer/Configuration/BeanDefinition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DIContainer.Configuration
+{
+    public class BeanDefinition
+    {
+        public IEnumerable<Type> Dependencies { get; private set; }
+        public IScope Scope { get; private set; }
+        public String Name { get; private set; }
+        public Type Type { get; private set; }
+        
+        public String Alias { get; private set; }
+
+        public BeanDefinition(IEnumerable<Type> dependencies, IScope scope, string name, Type type, String alias)
+        {
+            Dependencies = dependencies;
+            Scope = scope;
+            Name = name;
+            Alias = alias;
+            Type = type;
+        }
+        public BeanDefinition(IEnumerable<Type> dependencies, IScope scope, string name, Type type)
+        {
+            Dependencies = dependencies;
+            Scope = scope;
+            Name = name;
+            Alias = name;
+            Type = type;
+        }
+    }
+}
---
+++ DIContainer/Configuration/DependencyConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DIContainer.Configuration
+{
+    public class DependencyConfiguration
+    {
+        public Dictionary<Type, List<BeanDefinition>> BeanDefintions { get; private set; }
+
+        public DependencyConfiguration()
+        {
+            this.BeanDefintions = new Dictionary<Type, List<BeanDefinition>>();
+        }
+        
+        public void Register<I, T>(IScope scope)
+        {
+            Type type = typeof(I);
+            Type implType = typeof(T);
+            Register(type, implType, scope, implType.Name);
+        }
+
+        public void Register<I, T>(IScope scope, string name)
+        {
+            Register(typeof(I), typeof(T), scope, name);
+        }
+
+        public void Register(Type basicType, Type implType, IScope scope)
+        {
+            Register(basicType, implType, scope, implType.Name);
+        }
+
+        public void Register(Type basicType, Type implType, IScope scope, string name)
+        {
+            ConstructorInfo[] constructors = implType.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+            var maxParamConstructorInfo = constructors.MaxBy(info => info.GetParameters().Length);
+            IEnumerable<Type> referenceDependencyTypes = maxParamConstructorInfo
+                .GetParameters()
+                .Where(param => !param.GetType().IsPrimitive)
+                .Select(param => param.ParameterType);
+            var beanDefinition = new BeanDefinition(referenceDependencyTypes, scope, name, implType);
+            if (!BeanDefintions.ContainsKey(basicType))
+            {
+                BeanDefintions.Add(basicType, new List<BeanDefinition>());
+            }
+            BeanDefintions[basicType].Add(beanDefinition);
+        }
+    }
+}
