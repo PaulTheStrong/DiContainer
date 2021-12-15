b85fe5445bc5a372f7e87b6e9e82bdfdadb2f93e
PaulTheStrong <98765432123456789r@gmail.com>
2021-12-15 17:04:10 +0300

    context dependency resolver

---
+++ DIContainer/Context.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using DIContainer.Configuration;
+
+namespace DIContainer
+{
+    public class Context : IContext
+    {
+        private IBeanFactory _beanFactory;
+
+        public Context(DependencyConfiguration dependencyConfiguration)
+        {
+            _beanFactory = new BeanFactory(dependencyConfiguration);
+        }
+
+        public T Resolve<T>()
+        {
+            var type = typeof(T);
+            return (T) _beanFactory.GetBean(type);
+        }
+
+        public T Resolve<T>(string name)
+        {
+            var type = typeof(T);
+            return (T) _beanFactory.GetBean(type, name);
+        }
+    }
+}
---
+++ DIContainer/IContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DIContainer
+{
+    public interface IContext
+    {
+        T Resolve<T>();
+    }
+}
